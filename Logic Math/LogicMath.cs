12e91b78da07e8074d5b456e92ef576a858d7705
P0lariss <Yunissurchy@gmail.com>
2021-07-09 05:23:41 +0300

    Pushing LMath

---
+++ Logic Math/LogicMath.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Logic_Math
+{
+    public class LogicMath
+    {
+
+        public char connective;
+        public char[] p;
+        public char[] q;
+        public char[] n;
+
+
+        public void AND()
+        {
+            p = new char[] { 'T', 'T', 'F', 'F' };
+            q = new char[] { 'T', 'F', 'T', 'F' };
+
+            Console.WriteLine($"P  Q  P {connective} Q");
+            for (int i = 0; i < p.Length; i++)
+            {
+                Console.WriteLine($"{p[i]}  {q[i]} {CheckAND(p[i],q[i])}");
+            }
+            
+        }
+        public void OR()
+        {
+            p = new char[] { 'T', 'T', 'F', 'F' };
+            q = new char[] { 'T', 'F', 'T', 'F' };
+
+            Console.WriteLine($"P  Q  P {connective} Q");
+            for (int i = 0; i < p.Length; i++)
+            {
+                Console.WriteLine($"{p[i]}  {q[i]} {CheckOR(p[i],q[i])}");
+            }
+
+        }
+        private bool CheckAND(char p, char q)
+        {
+            if(p == 'T' && q == 'T')
+                return true;
+            else
+                return false;
+
+        }
+        private bool CheckOR(char p, char q)
+        {
+            if (p == 'T' || q == 'T')
+                return true;
+            else
+                return false;
+        }
+        public void Negation()
+        {
+            n = new char[] { 'T', 'F' };
+
+            Console.WriteLine($"N  {connective}N");
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                Console.WriteLine($"{p[i]}  {DoNegation(n[i])}");
+            }
+        }
+        private char DoNegation(char n)
+        {
+            if (n == 'T')
+                return 'F';
+            else
+                return 'T';
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MyProgram
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            Menue();
+        }
+        static void Menue()
+        {
+            int choice = 0;
+            Logic_Math.LogicMath lm = new Logic_Math.LogicMath();
+            Console.WriteLine("Welcome to Truth Table for connectives");
+            Console.WriteLine("**************************************");
+            do
+            {
+                
+                
+                Console.WriteLine("1.AND");
+                Console.WriteLine("2.OR");
+                Console.WriteLine("3.Negation");
+                Console.WriteLine("4.Exit");
+
+                choice = Int32.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        lm.connective = '^';
+                        lm.AND();
+                        break;
+                    case 2:
+                        lm.connective = 'v';
+                        lm.OR();
+                        break;
+                    case 3:
+                        lm.connective = '~';
+                        lm.Negation();
+                        break;
+                    default:
+                        Console.WriteLine("Sorry the choice invalid.");
+                        break;
+                }
+               
+            } while (choice != 4);
+        }
+    }
+  
+     
+}
